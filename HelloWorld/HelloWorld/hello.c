177da519b15f351631c6a7ff3f2fb7ab824a62a2
Christopher Zhang <christopherzhang3@gmail.com>
2013-12-24 15:10:24 -0500

    Check-in.

---
+++ HelloWorld/HelloWorld/hello.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/* 
+Experiment with escape sequences 
+*/
+main() {
+	// New-line
+	printf("New-line\n");
+	printf("Hello, World!\nTake me to your leader."); 
+	// Tab
+	printf("\n\nTab\n");
+	printf("Hello, World!\tTake me to your leader.");
+	// Backspace
+	printf("\n\nBackspace\n");
+	printf("Hello, World!\bTake me to your leader.");
+	// Double-quote
+	printf("\n\nDouble-quote\n");
+	printf("Hello, World!\"Take me to your leader.");
+	// Back-slash
+	printf("\n\nBack-slash\n");
+	printf("Hello, World!\\Take me to your leader.");
+	// Bad escape-sequence
+	printf("\n\nBad escape-sequence\n");
+	printf("Hello, World!\cTake me to your leader.");
+	getch();
+}
