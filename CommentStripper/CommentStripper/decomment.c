0e5e00b8f3f64299ae5321eb9a4ae447c30a88a4
Christopher Zhang <christopherzhang3@gmail.com>
2013-12-29 13:54:31 -0500

    Exercise 1-22.

---
+++ CommentStripper/CommentStripper/decomment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+(Exercise 1-23)
+Remove all comments from a C program. Don't forget to handle quoted strings and char
+constants properly. C comments do NOT nest.
+*/
+int main() {
+	// Go line by line
+	return 0;
+}
--- FoldLines/FoldLines/folder.c
+++ FoldLines/FoldLines/folder.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#define MAXLINE	1000
+#define COL_WIDTH 5
 
 /*
 (Exercise 1-22)
@@ -6,12 +8,46 @@ Fold long input lines into 2 or more shorter lines after the last non-blank char
 that occurs.
 */
 int main() {
+	char line[MAXLINE];
+	int len;
+	while ((len = getline(line)) > 0) {
+		if (len <= COL_WIDTH)
+			printf(line); // Phew! No folding needed
+		else { // Uh-oh! Needs word-wrapping
+			int unModified = len; // Amount that is unmodified chars will determine whether 
+			int endlineIndex = COL_WIDTH - 1;
+			while (unModified > COL_WIDTH) {
+				int change = 0;
+				while (endlineIndex == ' ') {
+					--endlineIndex;
+					++change;
+				}
+				for (int i = (endlineIndex - (COL_WIDTH - 1)); i <= endlineIndex; ++i) {
+					if (i < 0)
+						i = 0;
+					putchar(line[i]);
+				}
+				putchar('\n');
+				unModified -= COL_WIDTH - change;
+				endlineIndex += COL_WIDTH;
+			}
+			for (int i = endlineIndex - (COL_WIDTH - 1); i <= endlineIndex && line[i] != '\0'; ++i) {
+				putchar(line[i]);
+			}
+		}
+	}
+	getch();
 	return 0;
 }
 
-// Initialize a counter that will RESET everytime a newline char is encountered
-// If it reaches the given column-width
-	// Find the last char that is not a blank
-		// Insert a newline character after it
-		// Follow it with an escape \0 character
-		
+int getline(char line[]) {
+	int c, i;
+	for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+		line[i] = c;
+	if (c == '\n') {
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\0';
+	return i;
+}
